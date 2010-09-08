00c8d682171b71590e27e1f92e80b43127d99a2c
Valery V. Vorotyntsev <valery.vv@gmail.com>
2010-09-08 17:22:00 +0300

    generate C code from "CTT" mapping
    + plugins/ctt.in: CallTransactionType mapping
    + gen-ctt.sh: C structures for string-to-numeric CTT conversions and backwards

---
+++ gen-ctt.sh
@@ -0,0 +1,65 @@
+#!/bin/sh
+
+massage_input() {
+    sed -e 's/#.*// # remove comments' \
+	-e 's/^[ \t]\+//  # delete leading whitespace' \
+	-e 's/[ \t]\+$//  # delete trailing whitespace' | \
+	tr \\t ' ' | tr -s ' '
+}
+
+check_format() {
+    local errline="$(massage_input <"$1" | \
+	grep -En -m1 -v '^([0-9]+ [a-zA-Z0-9]+)?$' | cut -d: -f1)"
+
+    if [ -n "$errline" ]; then
+	echo "$1:$errline: invalid format of input line" >&2
+	exit 1
+    fi
+}
+
+WRAP_LINES=1
+maybe_fold() {
+    if [ -n "$WRAP_LINES" -a "$WRAP_LINES" != '0' ]; then
+	tr \\n ' ' | fold -s -w70
+    else
+	cat
+    fi
+}
+
+[ $# -eq 1 ] || { echo "Usage: ${0##*/} SOURCE_FILE" >&2; exit 1; }
+
+SRC="$1"
+check_format $SRC
+
+TMP_CTT=`mktemp` TMP_SEQ=`mktemp`
+trap "rm $TMP_CTT $TMP_SEQ" 0
+
+massage_input <$SRC | grep -v '^$' >$TMP_CTT
+[ -s $TMP_CTT ] || { echo "$SRC: no useful data" >&2; exit 1; }
+
+cat <<EOF
+/*
+ * This file was generated from \`$SRC'
+ * `date -R`
+ */
+
+#include <stdint.h>
+
+struct {
+	const char *symbol;
+	uint8_t num;
+} ctt_sym2num[] = {
+	{ NULL, 0 },
+EOF
+sort -k2 $TMP_CTT | awk '{ printf("\t{ \"%s\", %d },\n", $2, $1) }'
+echo '};'
+echo
+
+seq 0 $(tail -n1 $TMP_CTT | cut -d' ' -f1) | sort >$TMP_SEQ
+
+echo 'uint8_t ctt_num2sym[] = {'
+sort -k1,1 $TMP_CTT | join -a1 $TMP_SEQ - | sort -k2 | \
+    awk '{ print $1, (NF > 1) ? ++i "," : "0," }' | sort -n -k1,1 | \
+    cut -d' ' -f2 | head -c-2 | maybe_fold | sed 's/^/\t/'
+echo
+echo '};'
